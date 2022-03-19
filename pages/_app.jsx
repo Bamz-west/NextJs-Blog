112e29712251e0a7e2a3e26348a5efda9c751c84
bamz-west <fabamiseadeolu@gmail.com>
2022-03-19 15:57:58 +0100

    adding env file

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,9 +1,9 @@
 import React, { useEffect, useState } from 'react';
 import { Layout } from '../components';
-import type { AppProps } from 'next/app';
+import { AppProps } from 'next/app';
 import "../styles/globals.scss";
 
-function MyApp({ Component, pageProps }: AppProps) {
+function MyApp({ Component, pageProps }, AppProps) {
   return (
     <Layout>
       <Component {...pageProps} />
